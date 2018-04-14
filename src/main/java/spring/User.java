4c6986b84fa59f166c979cb79ebd4af0181ee157
OneCallAway <1054590332@qq.com>
2018-04-15 02:37:11 +0800

    Initial commit

---
+++ src/main/java/spring/User.java
@@ -0,0 +1,30 @@
+package spring;
+
+/**
+ * @author OneCallAway
+ * @create 2018-04-14 下午10:33
+ * @desc springIOC
+ **/
+
+public class User {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/spring/spring1.java
@@ -0,0 +1,16 @@
+package spring;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author OneCallAway
+ * @create 2018-04-11 下午7:17
+ * @desc test1
+ **/
+
+public class spring1 {
+    @Test
+    public void testIOC() throws Exception{
+
+    }
+}
